aa8e7117dda90139a3a84e4ff14054005b7a925a
pendragon888 <pendragon888@hotmail.com>
2026-08-20 17:18:50 +0100

    Update whatWeDo test to check for additional content

--- lib/pages/whatWeDo/whatWeDo.page.ts
+++ lib/pages/whatWeDo/whatWeDo.page.ts
@@ -4,11 +4,13 @@ export class WhatWeDoPage {
     readonly page: Page
     readonly headerModuleHeading: Locator
     readonly headerModuleContent: Locator
+    readonly ceoImage: Locator
 
     constructor(page:Page) {
         this.page = page
         this.headerModuleHeading = page.locator('.page-header-module__heading')
         this.headerModuleContent = page.locator('.page-header-module__content')
+        this.ceoImage = page.locator('.quote-image > img')
     }
 
     async goto(){
--- tests/whatWeDo/whatWeDo.spec.ts
+++ tests/whatWeDo/whatWeDo.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@fixtures/pages.fixture'
 
 test.describe("Visit What we do page for Protas", ()=>{
-        test('Validate What we do page is present', async ({ whatWeDoPage }) => {
+        test('Validate What we do page is present', async ({ whatWeDoPage, page }) => {
         await whatWeDoPage.goto()
         await expect(whatWeDoPage.headerModuleHeading).toContainText('We’re enabling clinical trials to succeed')
         await expect(whatWeDoPage.headerModuleContent).toContainText('There is a pressing need for better ways to detect, prevent and treat common and life-threatening diseases, to save lives, improve health and relieve pressure on healthcare systems around the world. But large-scale, randomised controlled trials (RCTs) have become excessively complex and unsustainably expensive, failing to generate the evidence needed for sound decision making. That’s where we come in.')
+        await expect(page.getByText('We launched as an independent not-for-profit in 2022 to drive change in clinical trials, specifically targeting common and life-threatening diseases because of the impact they have for millions of people worldwide.')).toBeVisible()
+        await expect(whatWeDoPage.ceoImage).toBeVisible()
     })
 })

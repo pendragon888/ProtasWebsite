1e9dec32b1aa040b19560a23072a9394dda19771
pendragon888 <pendragon888@hotmail.com>
2026-08-20 15:32:53 +0100

    Add tests to check for validation message and to fill out enquiry form

--- lib/pages/getInTouch/getInTouch.page.ts
+++ lib/pages/getInTouch/getInTouch.page.ts
@@ -5,10 +5,36 @@ export class GetInTouchPage {
     readonly headerModuleHeading: Locator
     readonly headerModuleContent: Locator
 
+    // Enquiry Fields
+    readonly firstName: Locator
+    readonly surname: Locator
+    readonly email: Locator
+    readonly phone: Locator
+    readonly sector: Locator
+    readonly country: Locator
+    readonly reason: Locator
+    readonly message: Locator
+    readonly submit: Locator
+    readonly consent: Locator
+    readonly validationError: Locator
+
     constructor(page:Page) {
         this.page = page
         this.headerModuleHeading = page.locator('.page-header-module__heading')
         this.headerModuleContent = page.locator('.page-header-module__content')
+
+        // Enquiry Fields
+        this.firstName = page.getByRole('textbox', { name: 'First name' })
+        this.surname = page.getByRole('textbox', { name: 'Surname' })
+        this.email = page.getByRole('textbox', { name: 'Email' })
+        this.phone = page.getByRole('textbox', { name: 'Phone' })
+        this.sector = page.getByLabel('Sector(Required)')
+        this.country = page.getByLabel('Country(Required)')
+        this.reason = page.getByLabel('Reason for enquiry(Required)')
+        this.message = page.getByRole('textbox', { name: 'Message' })
+        this.consent = page.getByText('I agree to the privacy policy.')
+        this.submit = page.getByRole('button', { name: 'Submit', exact: true })
+        this.validationError = page.locator('#gform_2_validation_container')
     }
 
     async goto(){
--- tests/getInTouch/getInTouch.spec.ts
+++ tests/getInTouch/getInTouch.spec.ts
@@ -1,9 +1,34 @@
 import { test, expect } from '@fixtures/pages.fixture'
+import { formData } from '@datafactory/enquiryData.json'
 
 test.describe("Visit Get in touch page for Protas", ()=>{
-        test('Validate Get in touch page is present', async ({ getInTouchPage }) => {
+    test.beforeEach(async ({ getInTouchPage }) => {
         await getInTouchPage.goto()
+    })
+
+    test('Validate Get in touch page is present', async ({ getInTouchPage }) => {
         await expect(getInTouchPage.headerModuleHeading).toContainText('Get in touch')
         await expect(getInTouchPage.headerModuleContent).toContainText('By transforming the quality and feasibility of clinical trials, we are enabling the development of innovative treatments for major causes of poor health.')
+    })
+
+    test('Check enquiry validation messages are shown', async ({ getInTouchPage }) => {
+        await getInTouchPage.firstName.clear()
+        await getInTouchPage.submit.click()
+        await expect (getInTouchPage.validationError).toBeVisible()
+        await expect (getInTouchPage.validationError).toContainText('There was a problem with your submission. Please review the fields below.')
+    })
+
+    test('Fill out enquiry form', async ({ getInTouchPage }) => {
+        await expect (getInTouchPage.firstName).toBeVisible()
+        await getInTouchPage.firstName.fill(formData.firstName)
+        await getInTouchPage.surname.fill(formData.surname)
+        await getInTouchPage.email.fill(formData.email)
+        await getInTouchPage.phone.fill(formData.phone)
+        await getInTouchPage.sector.selectOption(formData.sector)
+        await getInTouchPage.country.selectOption(formData.country)
+        await getInTouchPage.reason.selectOption(formData.reasonForEnquiry)
+        await getInTouchPage.message.fill(formData.message)
+        await getInTouchPage.consent.check()
+        // Do not submit due to production environment
     })
 })

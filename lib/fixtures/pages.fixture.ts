2b677f7bf633cf325b0bb1ddfa75ab58558fcb64
pendragon888 <pendragon888@hotmail.com>
2026-08-20 14:19:23 +0100

    Add page fixtures to navigate to main pages

--- lib/fixtures/pages.fixture.ts
+++ lib/fixtures/pages.fixture.ts
@@ -1,13 +1,48 @@
+import { NavigationPage } from "@pages/navigation/navigation.page";
 import { LandingPage } from "@pages/landing/landing.page";
+import { WhatWeDoPage } from "@pages/whatWeDo/whatWeDo.page";
+import { CantataPage } from "@pages/cantara/cantata.page";
+import { OurPeoplePage } from "@pages/ourPeople/ourPeople.page";
+import { NewsAndInsightsPage } from "@pages/newsAndInsights/newsAndInsights.page";
+import { WorkAtProtasPage } from "@pages/workAtProtas/workAtProtas.page";
+import { GetInTouchPage } from "@pages/getInTouch/getInTouch.page";
 import { test as baseTest } from "@playwright/test";
 
 type MyPages = {
-  landingPage: LandingPage;
+    navigationPage: NavigationPage;
+    landingPage: LandingPage;
+    whatWeDoPage: WhatWeDoPage;
+    cantataPage: CantataPage;
+    ourPeoplePage: OurPeoplePage;
+    newsAndInsightsPage: NewsAndInsightsPage;
+    workAtProtasPage: WorkAtProtasPage;
+    getInTouchPage: GetInTouchPage;
 };
 
 export const test = baseTest.extend<MyPages>({
+    navigationPage: async ({ page }, use) => {
+    await use(new NavigationPage(page));
+  },
     landingPage: async ({ page }, use) => {
     await use(new LandingPage(page));
+  },
+    whatWeDoPage: async ({ page }, use) => {
+    await use(new WhatWeDoPage(page));
+  },
+    cantataPage: async ({ page }, use) => {
+    await use(new CantataPage(page));
+  },
+    ourPeoplePage: async ({ page }, use) => {
+    await use(new OurPeoplePage(page));
+  },
+    newsAndInsightsPage: async ({ page }, use) => {
+    await use(new NewsAndInsightsPage(page));
+  },
+    workAtProtasPage: async ({ page }, use) => {
+    await use(new WorkAtProtasPage(page));
+  },
+    getInTouchPage: async ({ page }, use) => {
+    await use(new GetInTouchPage(page));
   },
 });
 
